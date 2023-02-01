ab49a1f8f4be220e670175edaa08133362a4f0b4
rookie-leo <proleonarpa@gmail.com>
2023-02-01 10:30:29 -0300

    initial project setup

---
+++ src/main/java/com/financeiro/receita/ReceitaApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.receita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitaApplication.class, args);
+	}
+
+}
