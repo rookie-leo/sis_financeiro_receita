6d0d3cdea633852460e6bf914b67b9ffa214b63f
rookie-leo <proleonardopa@gmail.com>
2023-03-01 17:00:24 -0300

    [ infra ] Conteinerização do projeto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into receita(descricao, valor, data_entrada) values ('Pagamento', 1445, '2022-12-12');
+insert into receita(descricao, valor, data_entrada) values ('Pagamento', 1445.45, '2022-12-12');
 insert into receita(descricao, valor, data_entrada) values ('Receita aluguel', 300, '2022-12-12');
 insert into receita(descricao, valor, data_entrada) values ('Receita poupança', 800, '2022-11-12');
 insert into receita(descricao, valor, data_entrada) values ('Descrição teste', 1445, '2023-01-12');
